e74319d0862f594dacc6a4c4f34d032f9c3f8d34
Mowafag <mmowfug@yahoo.com>
2021-11-25 12:12:24 +0100

    create user model

---
+++ src/modules/User/model.js
@@ -0,0 +1,45 @@
+import { Model, DataTypes } from "sequelize"
+import db from "../../config/database"
+
+class User extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        access_token: DataTypes.TEXT,
+        refresh_token: DataTypes.TEXT,
+      },
+      {
+        sequelize,
+        modelName: "User",
+      }
+    )
+  }
+
+  static associate(models) {
+    // define association here
+    // this.hasMany(models.Book, { as: 'books' })
+    return this
+  }
+}
+
+User.init(db.sequelize)
+
+const exclude = [
+  "access_token",
+  "refresh_token",
+  "updatedAt",
+  "createdAt",
+  "password",
+]
+
+User.beforeFind(async (user) => {
+  user.attributes = { exclude }
+})
+
+User.beforeUpdate(async (user) => {
+  user.attributes = { exclude }
+})
+
+export default User
